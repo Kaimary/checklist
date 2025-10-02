013d9fde6641d71e140406cafc9941e940b4ac6e
kaimary <kaimary1221@163.com>
2025-10-02 18:31:02 +0800

    update results

--- checklist/database_utils/execution.py
+++ checklist/database_utils/execution.py
@@ -33,6 +33,7 @@ def execute_sql(db_path: str, sql: str, fetch: Union[str, int] = "all") -> Any:
     """
     try:
         with sqlite3.connect(db_path) as conn:
+            conn.text_factory = lambda x: x.decode('utf-8', errors='replace')
             cursor = conn.cursor()
             cursor.execute(sql)
             if fetch == "all":
